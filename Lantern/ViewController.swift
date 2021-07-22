ee9602a445568abbcf30c11e47457b74a6b239c9
Nikolay Nikitin <nikitin.nikolay.v@gmail.com>
2021-07-22 19:46:53 +0300

    Initial Commit

---
+++ Lantern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lantern
+//
+//  Created by Николай Никитин on 22.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
